b95a1bf44ac373c52262e5afd2f6ed2af0e69128
imomaikel <imomaikeldev@gmail.com>
2024-03-05 22:45:36 +0100

    Added landing page Newsletter functionality

--- src/app/(marketing)/_components/Newsletter.tsx
+++ src/app/(marketing)/_components/Newsletter.tsx
@@ -1,32 +1,99 @@
+'use client';
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { trpc } from '@/components/providers/TRPC';
 import { Button } from '@/components/ui/button';
+import { errorToast } from '@/lib/utils';
+import { useState } from 'react';
 
-// TODO
 const Newsletter = () => {
+  const [value, setValue] = useState('');
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const { mutate: signUpForNewsletter, isLoading } = trpc.signUpForNewsletter.useMutation();
+
+  const handleClick = () => {
+    signUpForNewsletter(
+      { email: value },
+      {
+        onSuccess: ({ error, message, success }) => {
+          if (error) {
+            errorToast(message);
+          } else if (success) {
+            setIsDialogOpen(true);
+          }
+        },
+        onError: (error) => {
+          errorToast(error.data?.zodErrorList);
+        },
+      },
+    );
+  };
+
   return (
-    <div className="relative h-auto">
-      <div className="bg-footer bg-no-repeat bg-cover bg-center flex items-center justify-center lg:justify-end w-full min-h-[768px] relative px-8 lg:px-0">
-        <div className="bg-black/90 w-full lg:w-[650px] h-[500px] lg:mr-40 rounded-md text-white z-10 flex flex-col p-12 space-y-12">
-          <h3 className="text-primary tracking-wide font-normal mb-2 text-lg">Dołącz Do Nas</h3>
-          <h2 className="font-bold whitespace-pre-wrap text-2xl lg:text-4xl">
-            Podaj swojego e-mail&apos;a do Newsletter i bądź na bieżąco
-          </h2>
-          <div className="w-full rounded-full bg-gray-600 h-16 flex p-3">
-            <div className="w-full lg:w-3/5 h-full rounded-full">
-              <input
-                className="w-full h-full bg-transparent outline-none px-2 lg:px-4 text-xs lg:text-base"
-                placeholder="Podaj swój adres e-mail"
-              />
+    <>
+      {' '}
+      <div className="relative h-auto">
+        <div className="bg-footer bg-no-repeat bg-cover bg-center flex items-center justify-center lg:justify-end w-full min-h-[768px] relative px-8 lg:px-0">
+          <div className="bg-black/90 w-full lg:w-[650px] h-[500px] lg:mr-40 rounded-md text-white z-10 flex flex-col p-12 space-y-12">
+            <h3 className="text-primary tracking-wide font-normal mb-2 text-lg">Dołącz Do Nas</h3>
+            <h2 className="font-bold whitespace-pre-wrap text-2xl lg:text-4xl">
+              Podaj swój adres email i bądź na bieżąco.
+            </h2>
+
+            <div className="w-full rounded-full bg-gray-600 h-16 flex p-3">
+              <div className="w-full lg:w-3/5 h-full rounded-full">
+                <input
+                  className="w-full h-full bg-transparent outline-none px-2 lg:px-4 text-xs lg:text-base"
+                  placeholder="Podaj swój adres e-mail"
+                  type="email"
+                  disabled={isLoading}
+                  value={value}
+                  onChange={(e) => setValue(e.target.value)}
+                />
+              </div>
+              <Button
+                className="h-full w-2/5 rounded-full hidden lg:block text-black"
+                onClick={handleClick}
+                disabled={isLoading}
+              >
+                Zapisz
+              </Button>
             </div>
-            <Button className="h-full w-2/5 rounded-full hidden lg:block text-black">Zapisz</Button>
+            <Button
+              className="rounded-full shadow-md shadow-primary lg:hidden text-black"
+              size="2xl"
+              onClick={handleClick}
+              disabled={isLoading}
+            >
+              Zapisz
+            </Button>
           </div>
-          <Button className="rounded-full shadow-md shadow-primary lg:hidden text-black" size="2xl">
-            Zapisz
-          </Button>
+          <div className="absolute w-full h-full bg-black/20 top-0 z-0 backdrop-blur-sm" />
         </div>
-        <div className="absolute w-full h-full bg-black/20 top-0 z-0 backdrop-blur-sm" />
       </div>
-    </div>
+      {value && (
+        <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Newsletter</DialogTitle>
+            </DialogHeader>
+            <div>
+              <p>
+                Na podany adres email została wysłana wiadomość. Prosimy o kliknięcie w link znajdujący się w wiadomości
+                aby potwierdzić zapisanie się do newsletter&apos;a.
+              </p>
+            </div>
+            <DialogFooter>
+              <Button variant="ghost" onClick={() => setIsDialogOpen(false)}>
+                Zamknij
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      )}
+    </>
   );
 };
 
 export default Newsletter;
+
+// TODO Send mail
--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,7 +1,9 @@
+import { z } from 'zod';
 import { publicProcedure, router } from '../trpc';
 import { basketRouter } from './basketRouter';
 import { panelRouter } from './panelRouter';
 import { shopRouter } from './shopRouter';
+import { handlePrismaError } from './errors';
 
 export const appRouter = router({
   panel: panelRouter,
@@ -41,6 +43,26 @@ export const appRouter = router({
 
     return tags ?? null;
   }),
+  signUpForNewsletter: publicProcedure
+    .input(z.object({ email: z.string().email({ message: 'Podaj prawidłowy adres email.' }) }))
+    .mutation(async ({ ctx, input }) => {
+      const { email } = input;
+      const { prisma } = ctx;
+
+      try {
+        await prisma.newsletter.create({
+          data: {
+            email,
+          },
+        });
+        return { success: true };
+      } catch (error) {
+        if (handlePrismaError(error) === 'Object already exists') {
+          return { error: true, message: 'Podany użytkownik jest już zapisany.' };
+        }
+        return { error: true };
+      }
+    }),
 });
 
 export type AppRouter = typeof appRouter;
