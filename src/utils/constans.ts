03730e8beecf43a43d0604668c9a05461f7be4fb
imomaikel <imomaikeldev@gmail.com>
2024-03-26 21:53:18 +0100

    Removed unused variables

--- src/utils/constans.ts
+++ src/utils/constans.ts
@@ -7,7 +7,7 @@ import { RiTodoLine } from 'react-icons/ri';
 import { FaHashtag } from 'react-icons/fa';
 import { TiPlus } from 'react-icons/ti';
 import { FaEdit } from 'react-icons/fa';
-// TODO remove unused
+
 export const TStatuses: {
   value: PaymentStatus;
   label: string;
@@ -120,50 +120,6 @@ export const PANEL_TABS = [
         Icon: LiaShippingFastSolid,
         link: '/panel/zamowienia/dostawa',
       },
-    ],
-  },
-];
-
-export const TEMP_PRODUCTS = [
-  {
-    label: 'Olej rzepakowy',
-    price: 50,
-    image: '/TEMP/product.jpg',
-    description: [
-      'Obniża cholesterol',
-      'Wspiera układ krążenia',
-      'Duża zawartość witaminy E',
-      'Właściwości przeciwzapalne',
-      'Wysoka temperatura dymienia',
-      'Stabilny podczas gotowania i pieczenia',
-    ],
-  },
-  {
-    label: 'Olej z wiesiołka',
-    price: 65,
-    image: '/TEMP/product.jpg',
-    description: [
-      'Korzystny dla układu nerwowego.',
-      'Poprawia zdrowie stawów.',
-      'Wspomaga zdrowie kobiecego układu rozrodczego.',
-      'Wspomaga zdrowie serca.',
-      'Źródło antyoksydantów.',
-      'Przeciwdziała stanom zapalnym.',
-      'Zawartość kwasu GLA.',
-      'Poprawia zdrowie skóry.',
-    ],
-  },
-  {
-    label: 'Olej z konopii',
-    price: 30,
-    image: '/TEMP/product.jpg',
-    description: [
-      'Działa przeciwzapalnie.',
-      'Obniża ciśnienie krwi.',
-      'Chroni przed uszkodzeniem komórek i stresem oksydacyjnym.',
-      'Wspomaga trawienie.',
-      'Wspomaga zdrowie serca.',
-      'Bogaty w kwasy tłuszczowe.',
     ],
   },
 ];
