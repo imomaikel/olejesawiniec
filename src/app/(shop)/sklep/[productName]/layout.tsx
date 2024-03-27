eda79d92ce914a62d737832431cbe3b2d45ebfa3
imomaikel <imomaikeldev@gmail.com>
2024-03-27 12:36:56 +0100

    Added OpenGraph product preview

--- src/app/(shop)/sklep/[productName]/layout.tsx
+++ src/app/(shop)/sklep/[productName]/layout.tsx
@@ -2,14 +2,40 @@
 import { startCase } from 'lodash';
 import prisma from '@/lib/prisma';
 import { Metadata } from 'next';
+import { cache } from 'react';
+
+const getProduct = cache(async (productName: string) => {
+  const product = await prisma.product.findUnique({
+    where: { link: productName },
+  });
+  return product;
+});
 
 export async function generateMetadata({ params }: { params: { productName: string } }): Promise<Metadata> {
-  const product = await prisma.product.findUnique({
-    where: { link: params.productName },
-  });
+  const product = await getProduct(params.productName);
+  let description = '';
+
+  if (product?.description) {
+    try {
+      const data = JSON.parse(product.description);
+      const text = data.blocks.map((block: any) => block.text.replace(/[0-9][,.][0-9]/g, '').replace(/[0-9]./g, '.'));
+      for (const line of text) {
+        if (description.length > 500) break;
+        description += `${line}\n`;
+      }
+    } catch {}
+  }
 
   return {
     title: startCase(product?.label || 'Sklep'),
+    description,
+    ...(product?.mainPhoto && {
+      openGraph: {
+        images: {
+          url: product.mainPhoto,
+        },
+      },
+    }),
   };
 }
 
