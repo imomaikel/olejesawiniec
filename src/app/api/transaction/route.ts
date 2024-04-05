559c1131a2a76445dc4e7c31d9026f14cc194836
imomaikel <imomaikeldev@gmail.com>
2024-04-05 18:20:11 +0200

    Added Discord webhook on order payment finish

--- src/app/api/transaction/route.ts
+++ src/app/api/transaction/route.ts
@@ -1,5 +1,5 @@
+import { formatPrice, getConfig, isDayOrNight } from '@/lib/utils';
 import { getTransactionStatus } from '@/server/payments';
-import { getConfig, isDayOrNight } from '@/lib/utils';
 import { sendMail } from '@/server/mails/nodemailer';
 import { getPaymentMode } from '@/utils/paymentMode';
 import { NextRequest } from 'next/server';
@@ -7,6 +7,7 @@ import prisma from '@/lib/prisma';
 import crypto from 'node:crypto';
 
 const SHOP_SECRET = process.env.SHOP_SECRET;
+const WEBHOOK_URL = `https://discord.com/api/webhooks/${process.env.WEBHOOK_ID}/${process.env.WEBHOOK_TOKEN}`;
 
 const handler = async (req: NextRequest) => {
   try {
@@ -32,6 +33,7 @@ const handler = async (req: NextRequest) => {
     if (cmd === 'transactionStatusChanged') {
       const orderId = args;
       const updatedTransaction = await getTransactionStatus(orderId, getPaymentMode());
+
       if (updatedTransaction.statusCode === 200) {
         const { status } = updatedTransaction;
         const payment = await prisma.payment
@@ -41,6 +43,7 @@ const handler = async (req: NextRequest) => {
               status,
             },
             include: {
+              shipping: true,
               products: true,
             },
           })
@@ -48,56 +51,93 @@ const handler = async (req: NextRequest) => {
 
         if (payment) {
           const config = await getConfig();
-          const { email, products, shippingPrice, firstName: username, userId } = payment;
+          const { email, products, shippingPrice, firstName: username, userId, notified } = payment;
 
-          await sendMail(
-            'NewOrderMail',
-            {
-              dayOrNightTime: isDayOrNight(),
-              orderId: updatedTransaction.id,
-              orderUrl: `${process.env.NEXT_PUBLIC_PRODUCTION_URL}/zamowienie/${updatedTransaction.id}`,
-              products: products.map((product) => ({
-                label: product.productName,
-                price: product.productPrice,
-                quantity: product.productQuantity,
-                variant: `${product.productCapacity}${product.productUnit}`,
-              })),
-              supportMail: config.supportMail,
-              supportPhoneNumber: config.supportPhoneNumber,
-              username,
-              shippingPrice,
-            },
-            {
-              sendTo: email,
-              subject: 'Nowe zamówienie',
-            },
-          );
+          if (status === 'PositiveFinish' && !notified) {
+            const totalPrice = products.reduce((acc, curr) => (acc += curr.productPrice * curr.productQuantity), 0);
+            const embedProducts = products.map(
+              ({ productName, productCapacity, productUnit, productQuantity }, index) =>
+                `${index + 1}. **${productName}** ${productCapacity}${productUnit} x ${productQuantity}szt.`,
+            );
+            await fetch(WEBHOOK_URL, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                embeds: [
+                  {
+                    title: 'Nowe zamówienie!',
+                    description: `Skład zamówienia:\n${embedProducts.join('\n')}`,
+                    url: `${process.env.NEXT_PUBLIC_SERVER_URL}/panel/zamowienia/${payment.cashbillId}`,
+                    color: 3069299,
+                    footer: {
+                      text: `Zamówienie złożone przez: ${email}\nMetoda dostawy: ${
+                        payment.shipping?.method === 'COURIER' ? 'Kurier za pobraniem' : 'Paczkomat'
+                      }\nCałkowita cena produktów: ${formatPrice(totalPrice)}\nCałkowita cena dostawy: ${formatPrice(
+                        shippingPrice,
+                      )}\nCałkowita płatność: ${formatPrice(totalPrice + shippingPrice)}`,
+                    },
+                  },
+                ],
+              }),
+            }).then(async () => {
+              await prisma.payment.update({
+                where: { cashbillId: updatedTransaction.id },
+                data: {
+                  notified: true,
+                },
+              });
+            });
+            await sendMail(
+              'NewOrderMail',
+              {
+                dayOrNightTime: isDayOrNight(),
+                orderId: updatedTransaction.id,
+                orderUrl: `${process.env.NEXT_PUBLIC_PRODUCTION_URL}/zamowienie/${updatedTransaction.id}`,
+                products: products.map((product) => ({
+                  label: product.productName,
+                  price: product.productPrice,
+                  quantity: product.productQuantity,
+                  variant: `${product.productCapacity}${product.productUnit}`,
+                })),
+                supportMail: config.supportMail,
+                supportPhoneNumber: config.supportPhoneNumber,
+                username,
+                shippingPrice,
+              },
+              {
+                sendTo: email,
+                subject: 'Nowe zamówienie',
+              },
+            );
 
-          await prisma.user.update({
-            where: { id: userId },
-            data: {
-              basket: {
-                update: {
-                  variants: {
-                    set: [],
+            await prisma.user.update({
+              where: { id: userId },
+              data: {
+                basket: {
+                  update: {
+                    variants: {
+                      set: [],
+                    },
                   },
                 },
               },
-            },
-          });
+            });
 
-          for await (const product of products) {
-            if (!product.variantId) continue;
-            try {
-              await prisma.variant.update({
-                where: { id: product.variantId },
-                data: {
-                  stock: {
-                    decrement: product.productQuantity,
+            for await (const product of products) {
+              if (!product.variantId) continue;
+              try {
+                await prisma.variant.update({
+                  where: { id: product.variantId },
+                  data: {
+                    stock: {
+                      decrement: product.productQuantity,
+                    },
                   },
-                },
-              });
-            } catch {}
+                });
+              } catch {}
+            }
           }
         }
       }
